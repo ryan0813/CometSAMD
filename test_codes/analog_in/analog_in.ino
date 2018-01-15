145efbafbdb09c4bc05c5692b857ab1b2e506407
mbzabanal <mbzabanal@gmail.com>
2018-01-15 16:22:35 +0800

    Clean sample codes

--- test_codes/analog_in/analog_in.ino
+++ test_codes/analog_in/analog_in.ino
@@ -1,24 +1,13 @@
 void setup() {
-
   Serial.begin(9600);
   while(!Serial); Serial.println("Done Serial Init");
 }
 
 void loop() {
-  // Check Analog Input
-
   // Comet
   Serial.print("A0 "); Serial.println(analogRead(A0)* 3.3 / 1024.0); delay(1000);
   Serial.print("A1 "); Serial.println(analogRead(A1)* 3.3 / 1024.0); delay(1000);
   Serial.print("A2 "); Serial.println(analogRead(A2)* 3.3 / 1024.0); delay(1000);
   Serial.print("A3 "); Serial.println(analogRead(A3)* 3.3 / 1024.0); delay(1000);
   Serial.print("A4 "); Serial.println(analogRead(A4)* 3.3 / 1024.0); delay(1000);
-  /*
-  // Adafruit
-  Serial.print("A0 "); Serial.println(analogRead(A1)* 3.3 / 1024.0); delay(1000);
-  Serial.print("A1 "); Serial.println(analogRead(A2)* 3.3 / 1024.0); delay(1000);
-  Serial.print("A2 "); Serial.println(analogRead(A3)* 3.3 / 1024.0); delay(1000);
-  Serial.print("A3 "); Serial.println(analogRead(A4)* 3.3 / 1024.0); delay(1000);
-  //Serial.print("A4 "); Serial.println(analogRead(A4)* 3.3 / 1024.0); delay(1000);
-  */
 }
--- test_codes/digital_in/digital_in.ino
+++ test_codes/digital_in/digital_in.ino
@@ -8,9 +8,7 @@ void setup() {
   pinMode(3, INPUT);
   pinMode(4, INPUT);
   pinMode(5, INPUT);
-  // Skip 6 and 7 (6 is grounded, 7 connects to LED)
-  // pinMode(6, INPUT);
-  // pinMode(7, INPUT);
+  // Skip 6 and 7 (6 is grounded in the old version, 7 connects to LED)
   pinMode(8, INPUT);
 }
 
@@ -21,7 +19,5 @@ void loop() {
   Serial.print("D3 "); Serial.println(digitalRead(3)); delay(1000);
   Serial.print("D4 "); Serial.println(digitalRead(4)); delay(1000);
   Serial.print("D5 "); Serial.println(digitalRead(5)); delay(1000);
-  // Serial.print("D6 "); Serial.println(digitalRead(6)); delay(1000);
-  // Serial.print("D7 "); Serial.println(digitalRead(7)); delay(1000);
   Serial.print("D8 "); Serial.println(digitalRead(8)); delay(1000);
 }
